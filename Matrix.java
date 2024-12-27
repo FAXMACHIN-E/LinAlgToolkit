a84992a6e1f0f69b5d701593abc82784dc7ca774
adampochobut <natick5436@gmail.com>
2024-12-27 11:35:12 -0500

    Check RREF

--- Matrix.java
+++ Matrix.java
@@ -5,6 +5,7 @@ public class Matrix {
     //'struct' is the 2d matrix that stores all the values of the class
     private double[][] struct;
 
+
     //Creates a new, empty matrix with 'rows' amount of rows and 'columns' amount of columns
     public Matrix(int rows, int columns){
         this.struct = new double[rows][columns];
@@ -20,11 +21,11 @@ public class Matrix {
         }
     }
 
-    //TODO: Maybe remove this
     public Matrix(double[][] struct){
         this.struct = struct;
     }
 
+    //TODO
     public void augment(Vector b){
 
     }
@@ -99,91 +100,127 @@ public class Matrix {
         }
     }
 
-    //TODO: Finish zero row moving
-    public void rref(){
-        boolean[] pivotColumns = new boolean[getColumnAmount()];
+    /*Returns true if the matrix is in rref form, false if not */
+    //TODO: Check if rows of 0s at the bottom
+    public boolean inRref(){
 
-        //the two for loops interate over the matrix, it first iterates over the column to find the pivot
-        for(int col=0; col<getColumnAmount(); col++){
-            //boolean that checks if the pivot was found, starts 'false' at the beginning of each column
-            boolean pivotFound = false;
-            
-            //iterates over each row on and below the diagonal.
-            //we don't look above the diagonal as those are already pivot rows
-            for(int row=col; row<getRowAmount(); row++){
-                //Swaps the current row with every row below it until it is non-zero
-                //When it is non zero it stops searching and breaks the loop
-                if(getValue(row, col)!= 0.0){
-                    rowInterchange(row, col);
-                    pivotFound = true;
-                    pivotColumns[col] = true;
+        for(int row=0; row<getRowAmount(); row++){
+
+            //Iterates through each value in the row
+            for(int col=0; col<getColumnAmount(); col++){
+                //Finds the first non-zero value. If it is not 1.0, return false. Otherwise see if all other values in the pivot column are 0.
+                if(getValue(row, col) != 0){
+                    if(getValue(row, col) != 1.0){
+                        return false;
+                    }
+
+                    //If a value above or below the pivot position is not 0.0, return false.
+                    for(int i=0; i<getRowAmount(); i++){
+                        if(getValue(i, col) != 0.0 && i!=row){
+                            return false;
+                        }
+                    }
+
                     break;
-                }
+                }               
             }
 
-            //If there is no pivot, we end this iteration as we cannot reduce the rows below
-            if(!pivotFound){
-                continue;
-            }
-
-            //If there is a pivot, it will reduce all the values below the pivot to zero
-            //First we save the index of the row that contains the pivot
-            int pivotrow = col;
-
-            //We scale the pivot row so that it pivot is equal to 1
-            rowScale(pivotrow, 1/getValue(pivotrow, col));
-
-            //We iterate through all rows below the pivot row
-            for(int row=pivotrow+1; row<getRowAmount();row++){
-                if(getValue(row, col)!=0){
-                    //Find the common factor between them and use row replacement to make the position in the lower row 0
-                    double factor = -(getValue(row, col)/getValue(pivotrow, col));
-                    rowReplace(row, pivotrow, factor);
-                }
-            }
         }
 
-
-        //Going back up the matrix, starting from the lowest row, reducing up
-        for(int row=getRowAmount()-1;row>-1;row--){
-            //'pivot' will store the pivot position
-            int pivot = 0;
-            //iterates through the row to find the pivot index
-            for(int col=0; col<getColumnAmount(); col++){
-                if(getValue(row, col)==1.0){
-                    pivot = col;
-                }
-            }
-            //if a pivot exists, iterate through rows above to reduce them
-            if(pivot!=0){
-                for(int targetRow=row-1; targetRow>-1;targetRow--){
-                    double factor = -(getValue(targetRow, pivot)/getValue(row, pivot));
-                    rowReplace(targetRow, row, factor);
-                }
-            }
-        }
-
-        //This part moves all zero rows to the bottom
-        //'zeroRows' keeps track of the amount of zeroRows we have. We 1 row immediatly as it does not have to be checked as it doesn't matter whether its zero or not.
-        int zeroRows= 1;
-        for(int row=0; row<getRowAmount()-zeroRows;row++){
-            //We start off assuming each row is a row of zeros.
-            //If we find a non-zero value, make rowOfZeros false.
-            boolean rowOfZeros = true;
-            for(int col=0; col<getColumnAmount(); col++){
-                if(getValue(row, col)!= 0.0){
-                    rowOfZeros = false;
-                }
-            }
-
-            //Swaps zero rows with the lowest non-zero row.
-            //TODO:FUCK THERE'S AN ERROR BECAUSE WHAT IF THE LOWEST ROW IS A ZERO ROW????
-            if(rowOfZeros){
-                rowInterchange(row, getRowAmount()-zeroRows);
-            }
-        }
-        
+        return true;
     }
+
+    public void rref(){
+
+        //Repeat the reduction process until it is in rref
+        while(!this.inRref()){
+            //the two for loops interate over the matrix, it first iterates over the column to find the pivot
+            for(int col=0; col<getColumnAmount(); col++){
+                //boolean that checks if the pivot was found, starts 'false' at the beginning of each column
+                boolean pivotFound = false;
+                
+                //iterates over each row on and below the diagonal.
+                //we don't look above the diagonal as those are already pivot rows
+                for(int row=col; row<getRowAmount(); row++){
+                    //Swaps the current row with every row below it until it is non-zero
+                    //When it is non zero it stops searching and breaks the loop
+                    if(getValue(row, col)!= 0.0){
+                        rowInterchange(row, col);
+                        pivotFound = true;
+                        break;
+                    }
+                }
+
+                //If there is no pivot, we end this iteration as we cannot reduce the rows below
+                if(!pivotFound){
+                    continue;
+                }
+
+                //If there is a pivot, it will reduce all the values below the pivot to zero
+                //First we save the index of the row that contains the pivot
+                int pivotrow = col;
+
+                //We scale the pivot row so that it pivot is equal to 1
+                rowScale(pivotrow, 1/getValue(pivotrow, col));
+
+                //We iterate through all rows below the pivot row
+                for(int row=pivotrow+1; row<getRowAmount();row++){
+                    if(getValue(row, col)!=0){
+                        //Find the common factor between them and use row replacement to make the position in the lower row 0
+                        double factor = -(getValue(row, col)/getValue(pivotrow, col));
+                        rowReplace(row, pivotrow, factor);
+                    }
+                }
+            }
+
+
+            //Going back up the matrix, starting from the lowest row, reducing up
+            for(int row=getRowAmount()-1;row>-1;row--){
+                //'pivot' will store the pivot position
+                int pivot = 0;
+                //iterates through the row to find the pivot index
+                for(int col=0; col<getColumnAmount(); col++){
+                    if(getValue(row, col)==1.0){
+                        pivot = col;
+                        break;
+                    }
+                }
+                //if a pivot exists, iterate through rows above to reduce them
+                if(pivot!=0){
+                    for(int targetRow=row-1; targetRow>-1;targetRow--){
+                        double factor = -(getValue(targetRow, pivot)/getValue(row, pivot));
+                        rowReplace(targetRow, row, factor);
+                    }
+                }
+            }
+
+            //This is the part that puts the rows of zeros at the bottom
+            //Tracks the index of non-zero rows already moved to the top
+            int count = 0;
+            
+            for(int row=0; row<getRowAmount();row++){
+                //Start off by assuming that it is a row of zeros.
+                //If while iterating we find a non-zero value, we know it is not a row of zeros.
+                boolean rowOfZeros = true;
+                for(int col=0; col<getColumnAmount(); col++){
+                    if(getValue(row, col)!= 0.0){
+                        rowOfZeros = false;
+                        break;
+                    }
+                }
+
+                //If it is not a row of zeros, swap it with the top non-checked row.
+                if(!rowOfZeros){
+                    rowInterchange(row, count);
+                    count++;
+                }
+
+            }
+        }     
+    }
+        
+
+        
 
 
 
