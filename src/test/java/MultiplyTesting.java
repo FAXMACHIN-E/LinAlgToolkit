31507f7173ab48037690521f4c85742d927306cd
adampochobut <adampochobut@gmail.com>
2025-01-07 20:22:49 -0500

    Just a bit more testing

--- src/test/java/MultiplyTesting.java
+++ src/test/java/MultiplyTesting.java
@@ -1,9 +1,17 @@
 package src.test.java;
 
-import org.junit.Test;
-import org.junit.experimental.theories.DataPoints;
-import org.junit.experimental.theories.Theory;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import src.main.java.Matrix;
+import src.main.java.Vector;
+
+@RunWith(Theories.class)
 public class MultiplyTesting {
 
     @DataPoints
@@ -11,7 +19,25 @@ public class MultiplyTesting {
 
     @Theory
     public void testZeroMatrixMultiplication(int rows, int columns){
-        
+        Matrix matrix = new Matrix(rows, columns);
+        Vector vector = new Vector(columns);
+
+        int count = 0;
+        for(int i=0; i<matrix.getRowAmount(); i++){
+            for(int j=0; j<matrix.getColumnAmount(); j++){
+                matrix.setValue(i, j, count);
+                count++;
+            }
+        }
+        System.out.println(rows);
+        System.out.println(columns);
+
+        matrix.multiply(vector);
+
+        Vector zeroVector = new Vector(rows);
+
+        assertTrue(matrix.equals(zeroVector));
+
     }
 
     @Theory
